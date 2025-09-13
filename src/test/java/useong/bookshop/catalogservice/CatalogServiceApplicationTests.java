41d9e6369e8c0da38c4591736bd26e4d0e2a9167
iamuseong <iamuseong@gmail.com>
2025-09-13 16:45:50 +0900

    test 수정

--- src/test/java/useong/bookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/useong/bookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -5,13 +5,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import useong.bookshop.catalogservice.domain.Book;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("integration")
+@ActiveProfiles("test")
+@Import(TestcontainersConfiguration.class)
 class CatalogServiceApplicationTests {
 
 	@Autowired
